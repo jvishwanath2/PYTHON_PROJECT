097c48dc61a1810606ab986f85ebd7a3f0b92f9c
jvishwanath2 <jangavishwanath2@gmail.com>
2021-06-11 20:47:20 +0530

    added a new folder and copied files into that folder

---
+++ python_basics/if_else.py
@@ -0,0 +1,9 @@
+# strings in python is case sensitive
+# 20!=20 ---returns false
+# print(10<20)
+# relational operators -- >,<   logical operators--- and or
+if 10>5:
+  print('ten is greater than 5')
+a=5
+b=10
+print(a,b)
