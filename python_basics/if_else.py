4ef2d923479829913811ce2c5afa27eabdff5d3e
jvishwanath2 <jangavishwanath2@gmail.com>
2021-06-11 22:58:26 +0530

    worked on if else

--- python_basics/if_else.py
+++ python_basics/if_else.py
@@ -2,8 +2,30 @@
 # 20!=20 ---returns false
 # print(10<20)
 # relational operators -- >,<   logical operators--- and or
-if 10>5:
-  print('ten is greater than 5')
-a=5
-b=10
-print(a,b)
+# if 10>5:
+#   print('ten is greater than 5')
+# a=5
+# b=10
+# print(a,b)
+# use of ternary operators
+num=50
+num=num-20 if num>20 else num+100
+# print(num)
+# if 10>5:
+#   print('hi')
+# elif 10<5:
+#   print('bye')
+# # else:
+#    print('its ok')
+# input() -- this function by default takes string 
+#  so in order to take another data types use like int(input())
+# value_str='dxc'
+# value_list=list(value_str)
+# print(value_list)
+# list(1,2,3,4,5) -- error bcz list can handle atmost one argument like tuples, dictionaries sets etc..
+print(hex(50))
+print(bin(50))
+print(int('0b1010',2))
+# print(ord('a')) -- returns 97
+# print(chr(97)) -- returns 'a'
+print(complex(3,4))
